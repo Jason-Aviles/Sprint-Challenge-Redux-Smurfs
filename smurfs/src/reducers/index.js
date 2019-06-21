88636f2b9bab6ae26aecdb2f77b3d350113296a9
Jason Aviles <sandman2k18@outlook.com>
2019-06-21 12:58:56 -0400

    made the reducer for add and get

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,19 +1,77 @@
+import {
+  GET_SMURF,
+  GET_SMURF_FAIL,
+  GET_SMURF_SUCCESS,
+  ADD_SMURF,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_ERROR
+} from "../actions";
 /*
   Be sure to import in all of the action types from `../actions`
 */
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
 
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    //get smurfs start
+    case GET_SMURF:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: ""
+      };
+    case GET_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: false,
+        error: ""
+      };
+    case GET_SMURF_FAIL:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload
+      };
+        //get smurfs end
+
+        //add smurfs start
+    case ADD_SMURF:
+      return {
+        ...state,
+        addingSmurf: true,
+        error: ""
+      };
+    case ADD_SMURF_SUCCESS:
+      console.log(action.payload);
+
+      return {
+        ...state,
+        addingSmurf: false,
+        smurfs: action.payload
+      };
+    case ADD_SMURF_ERROR:
+      return {
+        ...state,
+        addingSmurf: false,
+        error: action.payload
+      };
+//add smurfs end
+
+// delete smurfs start
+
+    default:
+      return state;
+  }
+};
 /*
   You'll only need one smurf reducer for this project.
   Feel free to export it as a default and import as rootReducer. 
@@ -21,3 +79,4 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export default reducer;

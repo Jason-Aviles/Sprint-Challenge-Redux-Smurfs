805b240320a00c2de629297a527c026ef39542e3
Jason Aviles <sandman2k18@outlook.com>
2019-06-21 12:57:16 -0400

    completed the view file which connects to redux

---
+++ smurfs/src/view/SmurfListview.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
+import {getSmurf} from '../actions'
+import SmurfItem from '../components/SmurfItem'
+import AddSmurf from '../components/AddSmurf'
+class SmurfListview extends Component {
+    componentDidMount(){
+        this.props.getSmurf()
+    }
+    render() {
+        console.log(this.props.smurfs)
+        return (
+            <div>
+                {this.props.smurfs.map(
+                   ( smurf,i) => <SmurfItem key={i} smurf={smurf}/>
+                )}
+                <AddSmurf/>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = ({smurfs}) =>{
+    
+    return{
+      smurfs
+    }
+}
+
+export default connect(mapStateToProps,{getSmurf})(SmurfListview);
